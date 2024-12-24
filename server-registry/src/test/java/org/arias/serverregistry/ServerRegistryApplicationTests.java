0026eda590b999b3423fc887e505d1faedf9e008
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-12-24 11:03:26 -0500

    (UPDATE) Se registran las instancias en el server registry

---
+++ server-registry/src/test/java/org/arias/serverregistry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arias.serverregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
